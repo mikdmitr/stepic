c8da2cb0a9e707d4d7edef88e2653b2b75122b44
mikdmitr <39614405+mikdmitr@users.noreply.github.com>
2020-04-11 16:21:43 +0300

    initial commit

---
+++ Data/Pixel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    
+    public class Pixel
+    {
+        double Red;
+        double Green;
+        double Blue;
+
+        private double check(double val)
+        {
+                if ((val < 0) && (val > 1)) { throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", val)); } 
+                else return val;     
+        }
+
+        public double red
+        {
+            get { return Red; }
+            set {  Red = check(value);}
+        }
+        public double green
+        {
+            get { return Green; }
+            set { Green = check(value); }
+        }
+
+        public double blue
+        {
+            get { return Blue; }
+            set { Blue = check(value); }
+        }
+    }
+}
